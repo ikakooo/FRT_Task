aab3ab2168b3573fd3874798d0c5c8d480c43c83
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-03-30 13:01:25 +0400

    project initial

---
+++ FRT_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FRT_Task
+//
+//  Created by Irakli Chkhitunidzde on 30.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
