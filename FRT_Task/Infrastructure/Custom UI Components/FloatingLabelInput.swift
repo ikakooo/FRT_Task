1f88abde82d51132314ff11556e47770254ddcbd
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-04-01 12:43:45 +0400

    done button for text fild keyboard

--- FRT_Task/Infrastructure/Custom UI Components/FloatingLabelInput.swift
+++ FRT_Task/Infrastructure/Custom UI Components/FloatingLabelInput.swift
@@ -69,6 +69,7 @@ class FloatingLabelInput: UITextField {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        self.addDoneButtonOnKeyboard()
         self._placeholder = (self._placeholder != nil) ? self._placeholder : placeholder
         placeholder = self._placeholder // Make sure the placeholder is shown
         self.floatingLabel = UILabel(frame: CGRect.zero)
@@ -145,5 +146,22 @@ class FloatingLabelInput: UITextField {
             self.button.setImage(UIImage(named: "ic_hide"), for: .normal)
         }
     }
-}
+    
+    fileprivate func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
 
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc fileprivate func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
